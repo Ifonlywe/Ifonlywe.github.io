ab4b3a58dec08c95b8dc99b2ee4f1d23730eb837
Ifonlywe <167631598+Ifonlywe@users.noreply.github.com>
2024-04-21 13:20:00 +0200

    Update and rename createMerkle.js to createMerkle.mjs

--- createMerkle.mjs
+++ createMerkle.mjs
@@ -1,5 +1,8 @@
 import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
 import fs from "fs";
+import csv from 'csv-parser';
+
+const DataArray = []; // Define the array to store parsed data
 
 // (1)
 const values = [
@@ -7,9 +10,13 @@ const values = [
   ["0x2222222222222222222222222222222222222222", "2500000000000000000"]
 ];
 
+ console.log('Hardcoded data converted to array of arrays:', values);
+ console.log('CSV data converted to array of arrays:', DataArray);
+
 // (2)
+// const tree = StandardMerkleTree.of(DataArray, ["address", "uint256"]);
 const tree = StandardMerkleTree.of(values, ["address", "uint256"]);
-export default tree;
+
 // (3)
 console.log('Merkle Root:', tree.root);
 
